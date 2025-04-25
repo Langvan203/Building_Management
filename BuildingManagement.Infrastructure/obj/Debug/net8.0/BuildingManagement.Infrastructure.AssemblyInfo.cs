4dfd251932940227e978164eb5d70748c6df10b1
Langvan203 <langthaovan2003@gmail.com>
2025-04-26 00:06:41 +0700

    fix some error

--- BuildingManagement.Infrastructure/obj/Debug/net8.0/BuildingManagement.Infrastructure.AssemblyInfo.cs
+++ BuildingManagement.Infrastructure/obj/Debug/net8.0/BuildingManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79e0d1c85319c4ac93aff16100068771ca78511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51cfe46c4b6b1a7bb2250590b5ccc05e64632135")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
